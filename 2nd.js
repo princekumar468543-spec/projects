90facc3957e1046cb658bba3ba2c48ea7f76247c
PRINCE SHARMA <princekumar468543@gmail.com>
2026-02-09 09:05:13 +0530

    Add files via upload

--- 2nd.js
+++ 2nd.js
@@ -1,3 +1,41 @@
- let h1 =  document.createElement("h1");
- h1.textContent = "hello ji";
- document.body.append(h1);
+ let title = document.getElementById("title");
+let text = document.querySelector(".text");
+
+console.log(title);
+console.log(text);
+
+
+let h2 = document.createElement("h2");
+h2.textContent = "Created By JS";
+
+document.body.append(h2);
+
+h2.remove(); 
+
+
+let p = document.createElement("p");
+
+p.textContent = "This is Text";
+document.body.append(p);
+
+p.innerHTML = "<b>Bold Text</b>";
+
+
+let img = document.createElement("img");
+
+img.setAttribute("src","https://via.placeholder.com/150");
+img.setAttribute("alt","image");
+
+document.body.append(img);
+
+let box = document.createElement("div");
+box.textContent = "Box";
+
+box.style.width = "150px";
+box.style.height = "150px";
+box.style.background = "orange";
+
+document.body.append(box);
+
+
+
