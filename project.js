c820a9e4dc04d45983659586d0d255d2b5778515
PRINCE SHARMA <princekumar468543@gmail.com>
2026-02-18 23:51:33 +0530

    Add files via upload

--- project.js
+++ project.js
@@ -1,18 +1,28 @@
 var menu = document.querySelector(".navbar i");
-var close = document.querySelector(".sidebar i");
+var cross = document.querySelector(".sidebar i");
 var tl = gsap.timeline();
 
 tl.to(".sidebar",{
    right :0,
-   duration:1,
+   duration:0.8,
 })
 
 tl.from(".sidebar h4",{
 
    x:100,
-   stagger:1,
+   stagger:0.7,
    opacity: 0,
 })
 
 tl.pause();
+
+menu.addEventListener("click",function(){
+
+   tl.play();
+});
+
+cross.addEventListener("click",function(){
+   
+   tl.reverse();
+})
 
